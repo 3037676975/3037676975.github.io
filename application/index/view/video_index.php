519922ba1ce15bc4e0dbfb0c69d0acd3166688cd
3037676975 <73302133+3037676975@users.noreply.github.com>
2021-06-23 14:43:12 +0800

    1

--- application/index/view/video_index.php
+++
@@ -1,7 +0,0 @@
-<div class="layui-container">
-	<div class="url-box">
-		<input type="text" class="url-text" placeholder="需要解析的视频链接地址" spellcheck="false">
-		<div class="submit">解 析</div>
-	</div>
-	<div class="download-url-box"></div>
-</div>
